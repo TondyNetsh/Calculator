b41687e1b7a0ca1d10105b2064c5f4e068d101e1
TondyNetsh <rt.netsh@live.com>
2020-02-18 10:43:51 +0200

    Simple calculator part 1

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -20,25 +20,6 @@ public class Calculator {
 
     public static void main(String[] args) {
 
-//        Scanner nums=new Scanner(System.in);
-//        System.out.println("please add numbers seperated by \',\'");
-//        String num=nums.nextLine();
-//         num=num.replaceAll("[^\\d]","");
-//
-//        //boolean num2=num.matches("[\\d+]");
-//
-//        char[] ArraChar = num.toCharArray();
-//        int[] IntValues = new int[ArraChar.length];
-//
-//        for (int i = 0; i <ArraChar.length ; i++) {
-//              IntValues[i] = Character.getNumericValue(ArraChar[i]);
-//             //   System.out.println();
-//        }
-//
-//        System.out.println("=========Multiply===========");
-//        System.out.println(multiply(IntValues));
-//        System.out.println("=========Addition===========");
-//        System.out.println(add(IntValues));
         System.out.println("======== Multiply ==========");
         System.out.println(Calculator.multiply(1,2,3,4,5));
         System.out.println();
